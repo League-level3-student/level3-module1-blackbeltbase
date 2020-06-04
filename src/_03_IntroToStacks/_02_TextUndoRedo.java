ba407ca556e3f81dc8719ea8c0fb742761f1d876
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-06-04 11:31:01 -0700

    TextUndoRedo, HangMan(IP)

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -6,60 +6,77 @@ import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class _02_TextUndoRedo implements KeyListener {
 	Stack<Character> chars = new Stack<Character>();
-	Stack<Character> charsHolder = new Stack<Character>();
-	 static JFrame frame = new JFrame();
-	 static JPanel panel = new JPanel();
-	 static JLabel label = new JLabel();
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	static JLabel label = new JLabel();
+
 	public static void main(String[] args) {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel.add(label);
 		frame.add(panel);
-		_02_TextUndoRedo frames= new _02_TextUndoRedo();
+		frame.setSize(200, 200);
+		_02_TextUndoRedo frames = new _02_TextUndoRedo();
 		frame.pack();
 		frame.setVisible(true);
+		JOptionPane.showMessageDialog(null, "Type. use backspace to delete and control to undo");
 	}
+
 	public _02_TextUndoRedo() {
 		frame.addKeyListener(this);
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println(e.getKeyChar());
-		chars.push(e.getKeyChar());
-		charsHolder = chars;
+	
 		String text = "";
-for(int i = 0; i<chars.size();i++) {
-	text +=chars.pop().toString();
+
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			if (label.getText().length() != 0) {
+				text = label.getText().substring(0, label.getText().length() - 1);
+				chars.push(label.getText().charAt(label.getText().length() - 1));
+				System.out.println(label.getText().charAt(label.getText().length() - 1));
+			}
+		} else if (e.getKeyCode() == KeyEvent.VK_CONTROL) {
+			if (chars.size() != 0) {
+				text = label.getText() + chars.pop();
+			}
+		} else {
+			text = label.getText() + e.getKeyChar();
+		}
+		System.out.println(text);
 		label.setText(text);
-		System.out.println("textAdded");
-}
-frame.pack();
+		frame.pack();
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	/* 
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
 	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel. Save that deleted character onto a Stack of
+	 * Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
 	 * 
-	 * */
-	
-	
+	 */
+
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,59 @@
 package _04_HangMan;
 
-public class HangMan{
-	
-	
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener{
+	static Stack<String> words = new Stack<String>();
+	static JLabel[] life = new JLabel[5];
+	static Utilities wordReader = new Utilities();
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	static JLabel lives = new JLabel();
+	public static void main(String[] args) {
+		lives.setText("Lives:");
+		panel.add(lives);
+		for(int i = 0; i<life.length;i++) {
+			life[i] = new JLabel();
+			life[i].setText("<3");
+			panel.add(life[i]);
+		}
+		frame.add(panel);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		HangMan startup = new HangMan();
+		String userNum = JOptionPane.showInputDialog("Enter a number");
+		getWords(Integer.parseInt(userNum));
+	}
+	public HangMan() {
+		frame.addKeyListener(this);
+	}
+	static void getWords(int numberWords) {
+		for(int i = 0; i<numberWords;i++) {
+			words.push(wordReader.readRandomLineFromFile("dictionary.txt"));
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
