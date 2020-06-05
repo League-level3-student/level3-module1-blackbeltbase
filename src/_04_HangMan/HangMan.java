66dd21c4694e7e02fb6c8a4a52a683bfde7bbac2
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-06-05 11:25:06 -0700

    Hangman(IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -11,13 +11,20 @@ import javax.swing.JPanel;
 
 public class HangMan implements KeyListener{
 	static Stack<String> words = new Stack<String>();
-	static JLabel[] life = new JLabel[5];
-	static Utilities wordReader = new Utilities();
+ JLabel[] life = new JLabel[5];
+	static Utilities wordReader;
 	static JFrame frame = new JFrame();
 	static JPanel panel = new JPanel();
 	static JLabel lives = new JLabel();
 	static JLabel numLetters = new JLabel();
+	int hearts = 5;
+	String word = "";
+	CharSequence guess;
+	String text = "";
 	public static void main(String[] args) {
+		HangMan startup = new HangMan();
+	}
+	public HangMan() {
 		lives.setText("Lives:");
 		panel.add(lives);
 		for(int i = 0; i<life.length;i++) {
@@ -25,37 +32,72 @@ public class HangMan implements KeyListener{
 			life[i].setText("<3");
 			panel.add(life[i]);
 		}
+		panel.add(numLetters);
 		frame.add(panel);
-		frame.pack();
+		frame.setSize(500, 100);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		HangMan startup = new HangMan();
 		String userNum = JOptionPane.showInputDialog("Enter a number");
 		getWords(Integer.parseInt(userNum));
-	}
-	public HangMan() {
+
 		frame.addKeyListener(this);
+		wordReader = new Utilities();
+			word = words.pop();
+			System.out.println(word);
 		JOptionPane.showMessageDialog(null, "Guess letters to guess the word");
-		String text = "";
-		for(int i = 0; i< words.pop().length();i++) {
-			text += "_";
-		}
-		//add numLetters to frame
+		text = getWordLength(word);
+		numLetters.setText("Number of Letters: "+text);
+		
+		
 	}
-	static void getWords(int numberWords) {
+	String getWordLength(String word) {
+		String text = "";
+		for(int i = 0; i< word.length();i++) {
+			text += "|";
+		}
+		return text;
+	}
+  void getWords(int numberWords) {
 		for(int i = 0; i<numberWords;i++) {
 			words.push(wordReader.readRandomLineFromFile("dictionary.txt"));
 		}
 	}
+  
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		String characters = "";
+		 characters += e.getKeyChar();
+		String textHolder = "";
+		guess = characters;
+		int moreLetters = 0;
+		System.out.println(guess);
+		if(word.contains(guess)) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			System.out.println("correct");
+			System.out.println(e.getKeyChar());
+			for(int i = 0; i<word.length();i++) {
+				if(word.indexOf(e.getKeyChar())+moreLetters ==i) {
+					textHolder += e.getKeyChar();
+					System.out.println("letterAdded");
+				}
+				else {
+					textHolder += text.substring(i, i+1);
+				}
+				moreLetters++;
+			}
+			text = "";
+		}
+		else {
+			life[life.length-1].remove(life.length-1);;
+		}
+		text = textHolder;
+		numLetters.setText("Number of Letters: "+text);
+		frame.add(panel);
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
